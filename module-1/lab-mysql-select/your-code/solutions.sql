eebe28a3c2e2bfa8c4757ee1ec01f34d2a04f47e
Paula <postigorpaula@gmail.com>
2019-01-23 17:15:17 +0100

    agregado challenge 1 y 2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -4,7 +4,7 @@ LAB | My SQL Select
 ======================
 
 CHALLENGE 1 - Hay que unir tres tablas, dado que nos dicen que el total de rows debe ser igual al de la tabla titleauthor (25 rows), se debe hacer un inner join. 
-
+==============
 SELECT 
 	titleauthor.au_id AS 'AUTHOR ID',
     authors.au_lname AS 'LAST NAME',
@@ -18,6 +18,22 @@ FROM titleauthor
 
 
 CHALLENGE 2 - 
+=============
+SELECT 
+	titleauthor.au_id AS 'AUTHOR ID',
+    authors.au_lname AS 'LAST NAME',
+    authors.au_fname AS 'FIRST NAME', 
+    publishers.pub_name AS PUBLISHER,
+    COUNT(titles.title) AS 'TITLE COUNT'
+FROM titleauthor
+	INNER JOIN authors ON titleauthor.au_id = authors.au_id
+	INNER JOIN titles ON titleauthor.title_id = titles.title_id
+	INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
+GROUP BY titles.title
+
+
+CHALLENGE 3
+============
 
 
