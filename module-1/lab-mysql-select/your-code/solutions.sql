17f49b0393f18316a08c17507a959c0adbbeb5d0
Paula <postigorpaula@gmail.com>
2019-01-24 15:25:06 +0100

    acabado el challenge 3 y 4 y funcionando

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,10 +1,10 @@
 
-LAB | My SQL Select
+/* LAB | My SQL Select */
 
 ======================
 
-CHALLENGE 1 - Hay que unir tres tablas, dado que nos dicen que el total de rows debe ser igual al de la tabla titleauthor (25 rows), se debe hacer un inner join. 
-==============
+/*CHALLENGE 1 - Hay que unir tres tablas, dado que nos dicen que el total de rows debe ser igual al de la tabla titleauthor (25 rows), se debe hacer un inner join. 
+==============*/
 SELECT 
 	titleauthor.au_id AS 'AUTHOR ID',
     authors.au_lname AS 'LAST NAME',
@@ -17,8 +17,8 @@ FROM titleauthor
 	INNER JOIN publishers ON titles.pub_id = publishers.pub_id
 
 
-CHALLENGE 2 - 
-=============
+/*CHALLENGE 2 - 
+=============*/
 SELECT 
 	titleauthor.au_id AS 'AUTHOR ID',
     authors.au_lname AS 'LAST NAME',
@@ -33,23 +33,37 @@ FROM titleauthor
 GROUP BY titles.title
 
 
-CHALLENGE 3: NO FUNCIONA, DA ERROR
-============
+/*CHALLENGE 3:
+============*/
+
 SELECT 
 	titleauthor.au_id AS 'AUTHOR ID',
-    authors.au_lname AS 'LAST NAME',
-    authors.au_fname AS 'FINAL NAME',
-    sales.qty AS 'TOTAL'
-
+	authors.au_lname AS 'LAST NAME',
+	authors.au_fname AS 'FINAL NAME',
+	SUM(sales.qty) AS TOTAL
 FROM titleauthor
-	INNER JOIN authors ON titleauthor.au_id = authors.au_id
+    INNER JOIN authors ON titleauthor.au_id = authors.au_id
     INNER JOIN sales ON titleauthor.title_id = sales.title_id
 
+GROUP BY titleauthor.au_id
 ORDER BY TOTAL DESC
-
 LIMIT 3;
 
 
+/* CHALLENGE 4:
+
+================= */
+
+SELECT 	authors.au_id AS 'AUTHOR ID',
+		authors.au_lname AS 'LAST NAME',
+		authors.au_fname AS 'FINAL NAME',
+        SUM(IFNULL(titles.ytd_sales, 0)) AS TOTAL
+FROM authors
+	LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+    LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+    
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
 
 
 
