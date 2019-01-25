e0aa51f23338f6c6fb5445ff4d2398925dc375f7
Paula <postigorpaula@gmail.com>
2019-01-25 17:03:40 +0100

    Acabado el pair programming lab numpy deep dive

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,72 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print (np.__version__)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a= np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print (a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b= np.ones ((5,2,3))
 
 #6. Print b.
 
-
+print (b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+print (a.size == b.size)
 
-
+True
 
 #8. Are you able to add a and b? Why or why not?
 
-
+No se podrian sumar, ya que, aunque tienen mismo size, habria que cambiar la forma de una de ellas para que tuvieran mismo shape.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.reshape(b,(2,3,5))
 
+print(np.add (a,c))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add (a,c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print (a, d)
 
-
+La lista d es la lista a sumando a todos sus valores 1.
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = np.multiply (a, c)
 
 #13. Does e equal to a? Why or why not?
 
-
-
+Si, porque basicamente hemos multiplicado todos los valores por 1, ya que todos los valores de c son 1.
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = np.amax (d)
+d_min = np.amin (d)
+d_mean = np.mean (d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty ((2,3,5))
 
 
 """
@@ -75,6 +79,19 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for x in range (0,(d.shape[0])):
+  for y in range (0,(d.shape[1])):
+    for z in range (0,(d.shape[2])):
+      if d[x,y,z]> d_min and d[x,y,z] < d_mean:
+        f[x,y,z] = 25
+      if d[x,y,z]> d_mean and d[x,y,z] < d_max:
+        f[x,y,z] = 75
+      if d[x,y,z ]== d_mean:
+        f[x,y,z] = 50
+      if d[x,y,z ]== d_min:
+        f[x,y,z] = 0
+      if d[x,y,z ]== d_max:
+        f[x,y,z] = 100
 
 
 
@@ -98,7 +115,7 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print (d,f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +129,21 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+f = np.array(f).tolist()f = list (f)
+
+for x, e in enumerate(f):
+  for y, i in enumerate(e):
+    for z, q in enumerate(i):
+      if q == 0.0:
+        f[x][y][z] = "A"
+      if q == 25.0:
+        f[x][y][z] = "B"
+      if q == 50.0:
+        f[x][y][z] = "C"
+      if q == 75.0:
+        f[x][y][z] = "D"
+      if q == 100.0:
+        f[x][y][z] = "E"
+
+f = np.asarray(f)
+print (f)
